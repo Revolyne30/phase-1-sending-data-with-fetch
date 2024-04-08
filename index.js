f185b958132d1dd75d776cf6ea874cb8b8174ada
revolyne <revolyne.mwangi@student.moringaschool.com>
2024-04-09 00:33:05 +0300

    ajay

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    let userData = {
+      name: name,
+      email: email
+    };
+  
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML += `<p>${data.id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
+np  
